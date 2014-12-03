edcc624b47499ddb60e1c55b7ca66e1ab22e52c0
xxnations <xxnations@gmail.com>
2014-12-03 13:39:23 -0600

    *Header Modified

--- project/common/header.php
+++ project/common/header.php
@@ -4,49 +4,8 @@
 ?>
 <div id="headerdiv">
     <div id="headeroptionsmain" class="headeroptionsmain">
-            <table id="headertable">
-                <tr>
-                    
-                        <td> <span id="articlesubscribtion" class="headeroptions"><a href="index.php">{ Article Subscription }</a></span></td>
-                        <td> <span id="logout" class="headeroptions"><a href="logout.php">Log out</a></span></td>
-                        <td> <span id="myaccount" class="headeroptions"><a href="myaccount.php">My Account</a></span></td>
-                        <td> <span id="subscribe" class="headeroptions"><a href="subscribe.php">Subscribe</a></span></td>
-                   
-                </tr>
-            </table>
-         </div>
-             <div id="shoppingcartdiv">
-            <?php
-            if (empty($_SESSION['cart'])) {
-                $cartitems["size"]=0;
-                $_SESSION['cart'] = $cartitems;
-            } else {
-                echo "<a href=viewcart.php>";
-                echo "Cart Items = ".(sizeof($_SESSION['cart'])-1);
-                echo "</a>";
-            }
-            ?>
-        </div>
-        </div>
-<?php
-}
-else
-{
-    ?>
-<div id="headerdiv">
-    <div id="headeroptionsmain" class="headeroptionsmain">
-            <table id="headertable">
-                <tr>
-                    
-                        <td> <span id="articlesubscribtion" class="headeroptions"><a href="index.php">{ Article Subscription }</a></span></td>
-                        <td> <span id="register" class="headeroptions"><a href="register.php">Register</a></span></td>
-                        <td> <span id="login" class="headeroptions"><a href="login.php">Login</a></span></td>
-                        <td> <span id="forgotpassword" class="headeroptions"><a href="forgotpassword.php"> Forgot Password </a></span></td>
-                    
-                </tr>
-            </table>
-        </div>
-            <div id="shoppingcartdiv">
+       <div id="headerimage"> <img src="../project/images/title.png" alt="The Article Subscription"> </div>
+       <div id="shoppingcartdiv">
             <?php
             if (empty($_SESSION['cart'])) {
                 $cartitems["size"]=0;
@@ -61,6 +20,58 @@ else
             }
             ?>
         </div>
+        <div id="optionsbar">
+            <table id="headertable">
+                <tr>
+                    
+                        <td> <span id="articlesubscribtion" class="headeroptions"><a href="index.php">Home</a></span></td>
+                        <td> <span id="logout" class="headeroptions"><a href="logout.php">Log out</a></span></td>
+                        <td> <span id="myaccount" class="headeroptions"><a href="myaccount.php">My Account</a></span></td>
+                        <td> <span id="subscribe" class="headeroptions"><a href="subscribe.php">Subscribe</a></span></td>
+                   
+                </tr>
+            </table>
+        </div>
+         </div>
+            
+        </div>
+<?php
+}
+else
+{
+    ?>
+<div id="headerdiv">
+    <div id="headeroptionsmain" class="headeroptionsmain">
+        <div id="headerimage"> <img src="../project/images/title.png" alt="The Article Subscription"> </div>
+       <div id="shoppingcartdiv">
+            <?php
+            if (empty($_SESSION['cart'])) {
+                $cartitems["size"]=0;
+                $_SESSION['cart'] = $cartitems;
+                echo "<a href=viewcart.php>";
+                echo "Cart Items = 0";
+                echo "</a>";
+            } else {
+                echo "<a href=viewcart.php>";
+                echo "Cart Items = ".(sizeof($_SESSION['cart'])-1);
+                echo "</a>";
+            }
+            ?>
+        </div>
+        <div id="optionsbar">
+            <table id="headertable">
+                <tr>
+                    
+                        <td> <span id="articlesubscribtion" class="headeroptions"><a href="index.php">Home</a></span></td>
+                        <td> <span id="register" class="headeroptions"><a href="register.php">Register</a></span></td>
+                        <td> <span id="login" class="headeroptions"><a href="login.php">Login</a></span></td>
+                        <td> <span id="forgotpassword" class="headeroptions"><a href="forgotpassword.php"> Forgot Password </a></span></td>
+                    
+                </tr>
+            </table>
+        </div>
+        </div>
+            
         </div>
 <?php
 }
